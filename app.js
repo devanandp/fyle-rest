d8fedf8786df7cda944ca274f0ce94f567e1e72d
devanandp <deva051195@gmail.com>
2019-10-14 20:39:46 +0530

    Changes with respect to Practo

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv').config();
-const db = require('./Models/database')
-const routes = require('./Routes/branchDetails') 
-const tokenGeneration = require('./Authentication/tokenGeneration')
-const auth = require('./Authentication/authentication')
+const db = require('./src/Models/database')
+const routes = require('./src/Routes/branchDetails') 
+const tokenGeneration = require('./src/Authentication/tokenGeneration')
+const auth = require('./src/Authentication/authentication')
 var bodyParser = require('body-parser');
 
-const port = process.env.SERVER_PORT || 3020
+const port = process.env.PORT || 3020
 const app = express()
 
 app.use(bodyParser.json());
