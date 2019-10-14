5f36b470d06a5e7ac460475581122cd23bdc96ef
devanandp <deva051195@gmail.com>
2019-10-14 15:30:37 +0530

    Minor changes before deployment

--- src/Models/database.js
+++ src/Models/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
 const database = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER_NAME, process.env.DATABASE_PASSWORD, {
-    dialect: process.env.DATABASE_DIALECT,
+    dialect: 'postgres',
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     operatorsAliases: false
